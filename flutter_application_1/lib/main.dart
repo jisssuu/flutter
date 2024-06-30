791dc31c65fe6f24618b15f179507651c04a0927
jisssuu <wkdwltn1015@naver.com>
2024-06-30 15:24:33 +0900

    chap3

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -34,42 +34,42 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text("Test Title"),
       ),
       body: Container(
-        alignment: Alignment.topLeft,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        color: Colors.yellow,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width/2, // 사이즈 크기만큼 계산하여 하는 방법.
-                  height: 200,
-                  color: Colors.blue,
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width/2,
-                  height: 200,
-                  color: Colors.red,
-                ),
-              ],
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.blue,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 200,
-                  height: 200,
-                  color: Colors.green,
-                ),
-                Container(
-                  width: 200,
-                  height: 200,
-                  color: Colors.purple,
-                ),
-              ],
+            Container(
+              width: 200,
+              height: 200,
+              margin: const EdgeInsets.only(top: 50,left: 50),
+              color: Colors.red,
             ),
+            Container(
+              width: 200,
+              height: 200,
+              margin: const EdgeInsets.only(top: 100,left: 100),
+              color: Colors.green,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              margin: const EdgeInsets.only(top: 150,left: 150),
+              color: Colors.purple,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              margin: const EdgeInsets.only(top: 200,left: 200),
+              color: Colors.black,
+            )
           ],
-        )
+        ),
       ),
     );
   }
