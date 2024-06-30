c79f42d9d6e63c83772df6209ae1561b1e3da75f
jisssuu <wkdwltn1015@naver.com>
2024-06-30 16:03:36 +0900

    chap05

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -33,16 +33,39 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text("Test Title"),
       ),
-      body: Center(
-          child: GestureDetector(
-            onTap: () => print("GestuerDetector used."),
-            child: Container(
-              width: 200,
-              height: 200,
-              color:  Colors.yellow,
+      body: ListView(
+        scrollDirection: Axis.horizontal, // 기본값은 vertical !
+        children: [
+          postContainer(title:"Title 1",colorData: Colors.blue),
+          postContainer(title:"Title 2",colorData: Colors.green),
+          postContainer(title:"Title 3",colorData: Colors.pink),
+          postContainer(title:"Title 4",colorData: Colors.purple),
+          postContainer(title:"Title 5",colorData: Colors.blueGrey)
+        ]
+      ),
+    );
+  }
+
+  Widget postContainer({String title = '', Color colorData = Colors.blue}){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              title,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold
+              ),
             ),
           ),
-      )
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 200,
+            color: colorData
+          ),
+      ],
     );
   }
 }
