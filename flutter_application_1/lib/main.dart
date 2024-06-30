558657028491cecbc8ef791f93789dfbb5ba1501
jisssuu <wkdwltn1015@naver.com>
2024-06-30 15:34:00 +0900

    chap4

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -33,44 +33,16 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text("Test Title"),
       ),
-      body: Container(
-        color: Colors.yellow,
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Stack(
-          children: [
-            Container(
+      body: Center(
+          child: GestureDetector(
+            onTap: () => print("GestuerDetector used."),
+            child: Container(
               width: 200,
               height: 200,
-              color: Colors.blue,
+              color:  Colors.yellow,
             ),
-            Container(
-              width: 200,
-              height: 200,
-              margin: const EdgeInsets.only(top: 50,left: 50),
-              color: Colors.red,
-            ),
-            Container(
-              width: 200,
-              height: 200,
-              margin: const EdgeInsets.only(top: 100,left: 100),
-              color: Colors.green,
-            ),
-            Container(
-              width: 200,
-              height: 200,
-              margin: const EdgeInsets.only(top: 150,left: 150),
-              color: Colors.purple,
-            ),
-            Container(
-              width: 200,
-              height: 200,
-              margin: const EdgeInsets.only(top: 200,left: 200),
-              color: Colors.black,
-            )
-          ],
-        ),
-      ),
+          ),
+      )
     );
   }
 }
