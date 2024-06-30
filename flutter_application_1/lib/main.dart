6a4d845077e1a05d0413ffc5431fecfa587a8785
jisssuu <wkdwltn1015@naver.com>
2024-06-30 18:20:53 +0900

    chap11

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -29,39 +29,38 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   final postList = [
     {
-      "number" : "0",
-      "color" : Colors.amber,
+      "number": "0",
+      "color": Colors.amber,
     },
     {
-      "number" : "1",
-      "color" : Colors.black,
+      "number": "1",
+      "color": Colors.black,
     },
     {
-      "number" : "2",
-      "color" : Colors.blue,
+      "number": "2",
+      "color": Colors.blue,
     },
     {
-      "number" : "3",
-      "color" : Colors.purple,
+      "number": "3",
+      "color": Colors.purple,
     },
     {
-      "number" : "4",
-      "color" : Colors.pink,
+      "number": "4",
+      "color": Colors.pink,
     },
     {
-      "number" : "5",
-      "color" : Colors.grey,
+      "number": "5",
+      "color": Colors.grey,
     },
     {
-      "number" : "6",
-      "color" : Colors.brown,
+      "number": "6",
+      "color": Colors.brown,
     },
     {
-      "number" : "7",
-      "color" : Colors.lightGreen,
+      "number": "7",
+      "color": Colors.lightGreen,
     }
   ];
   @override
@@ -70,115 +69,31 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text("Test Title"),
       ),
-       body: Container(
-        child: Stack(
-          children: [
-
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              color:Colors.black26,
-            ),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: 150,
-                color: Colors.brown,
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: Container(
-                margin: EdgeInsets.only(left: 40,bottom: 150),
-                width: 100,
-                height: 100,
-                color: Colors.yellow,
-              ),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                margin: EdgeInsets.only(bottom: 200),
-                width: 300,
-                height: 70,
-                color: Colors.blue,
-              ),
-            )
-            // Align(
-            //   alignment: Alignment.topLeft,
-            //   child: Container(
-            //     width: 100,
-            //     height: 100,
-            //     color: Colors.amber,
-            //   ),
-            // ),
-            // Align(
-            //   alignment: Alignment.topCenter,
-            //   child: Container(
-            //     width: 100,
-            //     height: 100,
-            //     color: Colors.blue,
-            //   ),
-            // ),
-            // Align(
-            //   alignment: Alignment.topRight,
-            //   child: Container(
-            //     width: 100,
-            //     height: 100,
-            //     color: Colors.green,
-            //   ),
-            // ),Align(
-            //   alignment: Alignment.centerLeft,
-            //   child: Container(
-            //     width: 100,
-            //     height: 100,
-            //     color: Colors.amber,
-            //   ),
-            // ),
-            // Align(
-            //   alignment: Alignment.center,
-            //   child: Container(
-            //     width: 100,
-            //     height: 100,
-            //     color: Colors.blue,
-            //   ),
-            // ),
-            // Align(
-            //   alignment: Alignment.centerRight,
-            //   child: Container(
-            //     width: 100,
-            //     height: 100,
-            //     color: Colors.green,
-            //   ),
-            // ),Align(
-            //   alignment: Alignment.bottomLeft,
-            //   child: Container(
-            //     width: 100,
-            //     height: 100,
-            //     color: Colors.amber,
-            //   ),
-            // ),
-            // Align(
-            //   alignment: Alignment.bottomCenter,
-            //   child: Container(
-            //     width: 100,
-            //     height: 100,
-            //     color: Colors.blue,
-            //   ),
-            // ),
-            // Align(
-            //   alignment: Alignment.bottomRight,
-            //   child: Container(
-            //     width: 100,
-            //     height: 100,
-            //     color: Colors.green,
-            //   ),
-            // )
-          ],
+      body: Container(
+        child: Center(
+          child: TextButton(
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext con) {
+                    return AlertDialog(
+                      title: const Text("Dialog Title"),
+                      content: Container(
+                        child: const Text("Dialog Content"),
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: const Text("Close"),
+                        )
+                      ],
+                    );
+                  });
+            },
+            child: Text("Button"),
+          ),
         ),
-       ),
+      ),
     );
   }
-
 }
