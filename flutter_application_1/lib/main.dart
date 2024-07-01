08bd158373ae66649d9f1b33e73380a060a94a13
jisssuu <wkdwltn1015@naver.com>
2024-07-01 19:14:12 +0900

    chap21

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
-
+import 'package:dio/dio.dart';
+import 'package:flutter_application_1/product.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -29,22 +29,72 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-   static String youtubeId = 'sip2qsg1_WI';
 
-    final YoutubePlayerController _con = YoutubePlayerController(
-      initialVideoId: youtubeId,
-      flags: const YoutubePlayerFlags(autoPlay: false),
-    );
+  late Future<List<Product>> productList;
+  Dio dio = Dio();
   
+  @override
+  void initState() {
+    super.initState();
+    productList = getProductData();
+  }
+
+  Future<List<Product>> getProductData() async {
+    late List<Product> products;
+    try {
+      var response = await dio.get("https://dummyjson.com/products");
+      products = response.data['products']
+        .map<Product>((json) => Product.fromJson(json)).toList();
+    } catch (e) {
+      print('e');
+    }
+    return products;
+  }
+
+  Future<void> refreshData() async {
+    productList = getProductData();
+    setState(() {
+      
+    });
+  }
   @override
   Widget build(BuildContext context) { 
     return Scaffold(
       appBar: AppBar(
         title: const Text("Test Title"),
       ),
-     body: Container(
-      child: YoutubePlayer(
-        controller: _con,
+     body: RefreshIndicator(
+      onRefresh: () => refreshData(),
+      child: SingleChildScrollView(
+        physics: AlwaysScrollableScrollPhysics(),
+        child: FutureBuilder<List<Product>>(
+          future: productList,
+          builder: (BuildContext con,AsyncSnapshot snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(
+                  strokeWidth: 2,
+                ),
+               );
+            } else {
+              return ListView.builder(
+                shrinkWrap: true,
+                physics:  const NeverScrollableScrollPhysics(),
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext cont, int index) {
+                  var data = snapshot.data[index];
+                  return Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.black26),
+                    ),
+                    child: Text("${data.title}(${data.description})"),
+                  );
+                }
+              );
+            }
+          }
+        ),
       ),
      )
     );
