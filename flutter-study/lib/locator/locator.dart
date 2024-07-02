35d0f2470cd6ffede65bf0b7abc7c05fadda6955
jisssuu <wkdwltn1015@naver.com>
2024-07-02 19:43:44 +0900

    chap25

--- flutter-study/lib/locator/locator.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_application_1/service/album_service.dart';
-import 'package:get_it/get_it.dart';
-
-GetIt locator = GetIt.instance;
-
-initLocator() {
-  locator.registerLazySingleton<AlbumService>(() => AlbumServiceImplementation());
-}
