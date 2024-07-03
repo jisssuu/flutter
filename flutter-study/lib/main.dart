07ded0bd36aec8e2609421db041dca17185e066d
jisssuu <wkdwltn1015@naver.com>
2024-07-03 16:30:07 +0900

    chap27

--- flutter-study/lib/main.dart
+++ flutter-study/lib/main.dart
@@ -1,20 +1,23 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/counterController.dart';
+import 'package:flutter_application_1/studentController.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:get/instance_manager.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final Studentcontroller _con = Get.put(Studentcontroller());
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      title: 'GetX Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -22,28 +25,38 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('GetX Example'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment:  MainAxisAlignment.center,
-            children: [
-              GetBuilder<Countercontroller>(
-                init: Countercontroller(),
-                builder: (_) => Text("Current Count: ${Get.find<Countercontroller>().counter}"),
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+        body: ListView.builder(
+          itemCount: _con.studentList.length,
+          itemBuilder: (BuildContext context,int index) {
+            return Container(
+              margin: EdgeInsets.all(15),
+              padding:  EdgeInsets.all(15),
+              child: Column(
                 children: [
-                  TextButton(onPressed: () => Get.find<Countercontroller>().increase(),
-                  child: const Text("증가"),
+                  GetX<Studentcontroller>(
+                    builder: (_) => Text(
+                      "ID: ${_con.studentList[index]().studentId}, Name: ${_con.studentList[index]().studentName}, Grade: ${_con.studentList[index]().studentGrade}",
+                    ),
                   ),
-                    TextButton(onPressed: () => Get.find<Countercontroller>().decrease(),
-                  child: const Text("감소"),
-                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(onPressed: () => _con.updateStudentName(
+                        _con.newStudentNames[index],index
+                      ),
+                       child: const Text("이름 변경")
+                      ),
+                       TextButton(onPressed: () => _con.updateStudentGrade(
+                        _con.newStudnetGrades[index],index
+                      ),
+                       child: const Text("성적 변경")
+                      )
+                    ],
+                  )
                 ],
-               ),
-            ],
-          ),
+              ),
+            );
+          },
         ),
       ),
     );
---
+++ flutter-study/lib/studentModel.dart
@@ -0,0 +1,8 @@
+class Student{
+  int studentId;
+  String studentName;
+  String studentGrade;
+
+  Student({
+    this.studentId = 0, this.studentName = "", this.studentGrade = ""});
+}
