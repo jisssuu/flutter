f760ad95d476ea4c21f68bd437de159b0beffd93
jisssuu <wkdwltn1015@naver.com>
2024-07-08 18:16:55 +0900

    chap44

--- flutter-study/lib/main.dart
+++ flutter-study/lib/main.dart
@@ -1,110 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_keyboard/flutter_secure_keyboard.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
-      home: const MainPage(),
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
-      ),
+      title: 'Test App',
+      home: const TestView(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
-
-class MainPage extends StatefulWidget {
-  const MainPage({Key? key}) : super(key: key);
+class TestView extends StatefulWidget {
+  const TestView({super.key});
 
   @override
-  State<MainPage> createState() => _MainPageState();
+  State<TestView> createState() => _TestViewState();
 }
 
-class _MainPageState extends State<MainPage> {
-  List<String> friendList = [
-    "Andrew",
-    "Brian",
-    "Catherine",
-    "Wilson",
-    "Raul",
-    "Daniel",
-    "John",
-  ];
+class _TestViewState extends State<TestView> {
+  final _secureKeyboardController = SecureKeyboardController();
+
+  final _passwordEditor = TextEditingController();
+  final _passwordTextFieldFocusNode = FocusNode();
+
+  final _pinCodeEditor = TextEditingController();
+  final _pinCodeTextFieldFocusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Material 3 Design"),
-        centerTitle: true,
-      ),
-      body: ListView.builder(
-          itemCount: friendList.length,
-          itemBuilder: (context, index) {
-            return cardContainer(name: friendList[index], number: index + 1);
-          }),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showFloatingDialog();
-        },
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-
-  Widget cardContainer({String name = "", int number = 0}) {
-    return Card(
-      margin: const EdgeInsets.all(10),
-      child: ListTile(
-        title: Container(
-          width: MediaQuery.of(context).size.width,
-          padding: const EdgeInsets.all(12),
-          child: Text("$number. $name", style: const TextStyle(fontSize: 20)),
+    return WithSecureKeyboard(
+      controller: _secureKeyboardController, 
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Secure Keyboard Test"),
+          centerTitle: true,
         ),
-        subtitle: Row(
-          children: [
-            TextButton(
-              onPressed: () {},
-              child: const Text("Details"),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                _buildPasswordTextField(),
+                const SizedBox(height: 15.0,),
+                _buildPincodeTextField(),
+              ],
             ),
-            FilledButton.tonal(
-              onPressed: () {},
-              child: const Text("Follow"),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void showFloatingDialog() {
-    showDialog(
-      context: context,
-      barrierDismissible: true,
-      builder: (context) {
-        return AlertDialog(
-          content: const Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Center(child: Text("Do you want to add a friend?")),
-            ],
           ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text("No"),
-            ),
-            FilledButton(onPressed: () {}, child: const Text("Yes")),
-          ],
-        );
-      },
+        ),
+      )
+      );
+  }
+
+  Widget _buildPincodeTextField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text("Password"),
+        TextFormField(
+          controller: _passwordEditor,
+          focusNode: _passwordTextFieldFocusNode,
+          enableInteractiveSelection: false,
+          obscureText: true,
+          onTap: () {
+            _secureKeyboardController.show(
+              type: SecureKeyboardType.ALPHA_NUMERIC,
+              focusNode: _passwordTextFieldFocusNode,
+              initText: _passwordEditor.text,
+              hintText: "password",
+              onCharCodesChanged: (List<int> charCodes) {
+                _passwordEditor.text = String.fromCharCodes(charCodes);
+              },
+            );
+          },
+        )
+      ],
     );
+  }
+
+  Widget _buildPasswordTextField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text("PinCode"),
+        TextFormField(
+          controller: _pinCodeEditor,
+          focusNode: _pinCodeTextFieldFocusNode,
+          enableInteractiveSelection: false,
+          obscureText: true,
+          onTap: () {
+            _secureKeyboardController.show(
+              type: SecureKeyboardType.NUMERIC,
+              focusNode: _pinCodeTextFieldFocusNode,
+              initText: _pinCodeEditor.text,
+              hintText: "pinCode",
+              onDoneKeyPressed: (List<int> charCodes) {
+                _pinCodeEditor.text = String.fromCharCodes(charCodes);
+              },
+            );
+          },
+        )
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _secureKeyboardController.dispose();
+    _passwordEditor.dispose();
+    _pinCodeEditor.dispose();
   }
 }
